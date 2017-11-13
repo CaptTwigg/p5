92d661207ca7b12a07a73764c434899572be8872
Mike Hemmje Jahn <bob@Mike-Hemmje-Jahn-Air.local>
2017-11-13 12:27:59 +0100

    snow

--- Projekt/class/empty-example/sketch.js
+++ Projekt/class/empty-example/sketch.js
@@ -1,14 +1,19 @@
 let bubbles = [];
 
+
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(1440, 900);
 
 }
 
 function draw() {
   background(0);
 
-  bubbles.push(new bubble());
+  bubbles.push(new bubble(random(0, width), -10, 0));
+  bubbles.push(new bubble(random(0, width), -10, 0));
+  bubbles.push(new bubble(random(0, width), 900, 1));
+  bubbles.push(new bubble(random(0, width), 900, 1));
+
 
   for (let i = bubbles.length - 1; i > 0; i--) {
     bubbles[i].move();
@@ -20,12 +25,24 @@ function draw() {
 }
 
 class bubble {
-  constructor() {
-    this.x = 300;
-    this.y = 380;
+
+  constructor(x, y, up) {
+    this.x = x;
+    this.y = y;
+    this.r;
+    this.up = up;
     this.vx = random(-1, 1);
-    this.vy = random(-3, -1);
+    if (up == 1) {
+      this.vy = random(-1, -3);
+    } else {
+      this.vy = random(1, 3);
+    }
     this.alpha = 255;
+    this.red = random(0, 255);
+    this.blue = random(0, 255);
+    this.green = random(0, 255);
+    this.r = random(8, 17);
+
   }
 
   remove() {
@@ -34,15 +51,15 @@ class bubble {
 
   show() {
     noStroke();
-    fill(255, this.alpha);
-    ellipse(this.x, this.y, 16);
+    fill(this.red, this.green, this.blue, this.alpha);
+    ellipse(this.x, this.y, this.r);
   }
   move() {
     this.x += this.vx;
     this.y += this.vy;
-    this.alpha -= 5;
+    this.alpha -= random(0.1, 2);
   }
   pulse() {
-    this.r += random(-1, 1);
+    this.r = random(1, 16);
   }
 }
